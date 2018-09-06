2834f10f19ad0f232eaa6e2448bf11ead96fa3fb
Danila Stefan <Danilastefan69@yahoo.ro>
2018-09-06 19:47:54 +0300

    Adding NFC patch to Android.mk

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ LOCAL_PATH:= $(call my-dir)
 ########################################
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_PATH := $(TARGET_OUT_APPS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
